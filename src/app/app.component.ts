1f249be4302a5c3f2324101dec14cadb663b3750
Julia Martins <martinss.s.julia@gmail.com>
2024-01-02 15:15:59 -0500

    feat: created API connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
+  constructor(private weatherService: WeatherService) {}
+  
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Rio de Janeiro')
+    .subscribe({
+      next: (response) => {
+        console.log(response)
+      }
+    })
+  }
   title = 'WeatherApp';
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false, 
+    weatherApiBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'd5d6ab5206mshf1fae1a3c5c8793p128feejsn32f1fce9056c'
+};
